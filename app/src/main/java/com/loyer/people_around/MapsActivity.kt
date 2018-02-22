9a1a14d57ceb7f1f58e7c2827b9d9963cfea5109
celikrecep <recep_clk10@hotmail.com>
2018-02-22 20:29:15 +0300

    request on run time permission and show player location on the map

--- app/src/main/java/com/loyer/people_around/MapsActivity.kt
+++ app/src/main/java/com/loyer/people_around/MapsActivity.kt
@@ -1,18 +1,39 @@
 package com.loyer.people_around
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private val TAG = "PermissionDemo"
+    private val PERMISSIONS_REQUEST = 101
+    private val MIN_TIME_BW_UPDATES = (1000 * 60 * 1).toLong()
+    private val MIN_DISTANCE_CHANGE_FOR_UPDATES: Float = 10F
+    private val ZOOM_CAMERA = 18F
+    var mMap: GoogleMap? = null
+    var location: Location? = null
+    var locationManager: LocationManager? = null
+    var latitude: Double? = 0.0
+    var longitude: Double? = 0.0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +53,76 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val isGpsEnabled = locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+        if (isGpsEnabled){
+            locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER
+                    , MIN_TIME_BW_UPDATES
+                    , MIN_DISTANCE_CHANGE_FOR_UPDATES
+                    , locationListener)
+
+            if (locationManager != null){
+                location = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (location != null){
+
+                    latitude = location!!.latitude
+                    longitude = location!!.longitude
+                    var coordinates = LatLng(latitude!!, longitude!!)
+                    //we've added a marker to our location
+                    mMap!!.addMarker(MarkerOptions()
+                            .position(coordinates))
+                    mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates,ZOOM_CAMERA))
+
+                }
+            }
+        }
+
+    }
+
+    //When the user has responded to the permission request
+    // the onRequestPermissionsResult() method will be
+    // called on the activity
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+
+        when (requestCode) {
+            PERMISSIONS_REQUEST -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    Log.i(TAG, "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
+    //we Used for receiving notifications from the
+    // LocationManager when the location has changed
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+
+    //requesting permission from the user
+    private fun setupPermission(){
+        val permission = ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG,"Permission to recor denied")
+            makeRequest()
+        }
+    }
+
+    //a method added to class to request a specific permission from the user
+    private fun makeRequest(){
+        ActivityCompat.requestPermissions(this
+                , arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+                ,PERMISSIONS_REQUEST)
     }
 }
