49471db7ee8bea95701b069ecfe5485f529f83e8
celikrecep <recep_clk10@hotmail.com>
2018-02-23 22:12:07 +0300

    remove RegisterFragment,Registering new user with Firebase

--- app/src/main/java/com/loyer/people_around/LoginActivity.kt
+++ app/src/main/java/com/loyer/people_around/LoginActivity.kt
@@ -1,15 +1,19 @@
 package com.loyer.people_around
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    private var TAG = "Register"
+    private var isBegin: Boolean = false
     private var manager: FragmentManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,25 +22,13 @@ class LoginActivity : AppCompatActivity() {
         manager = supportFragmentManager
     }
 
-    fun register(view: View){
-        var fragmentRegister = FragmentRegister()
-
-        var transaction: FragmentTransaction =manager!!.beginTransaction()
-        transaction.add(R.id.loginActivity,fragmentRegister,"fragRegister")
-        transaction.commit()
-        header.visibility = View.GONE
-        container.visibility = View.GONE
-        Log.d("REGISTER","Button Pressed")
-
+     fun register(view: View){
+        val intent = Intent(this@LoginActivity,RegisterActivity::class.java)
+         startActivity(intent)
+         finish()
     }
 
-    fun signIn(view: View){
 
-        var fragmentRegister : FragmentRegister = manager!!.findFragmentByTag("fragRegister") as FragmentRegister
-        var transaction: FragmentTransaction = manager!!.beginTransaction()
-        transaction.remove(fragmentRegister)
-        transaction.commit()
-        header.visibility = View.VISIBLE
-        container.visibility = View.VISIBLE
-    }
+
+
 }
---
+++ app/src/main/java/com/loyer/people_around/RegisterActivity.kt
@@ -0,0 +1,121 @@
+package com.loyer.people_around
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    private val TAG = "Register"
+    private var eMail: EditText? = null
+    private var name:EditText? = null
+    private var mPassword: EditText? = null
+    private var mConfirmPassword: EditText? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        eMail = findViewById(R.id.edtRegisterEmail)
+        name = findViewById(R.id.edtName)
+        mPassword = findViewById(R.id.edtRegisterPassword)
+        mConfirmPassword = findViewById(R.id.edtRegisterConfirm)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+
+    fun signIn(view: View){
+
+        attemptRegistration()
+
+    }
+
+
+    private fun attemptRegistration(){
+
+        eMail?.error = null
+        eMail?.error = null
+
+        var email: String = eMail?.text!!.toString()
+        var password: String = mPassword?.text!!.toString()
+        var cancel = false
+        var focusView: View? = null
+
+
+
+        if(TextUtils.isEmpty(email)){
+            eMail?.error = getString(R.string.error_invalid_email)
+            focusView = eMail
+            cancel = true
+        }
+
+        if(!TextUtils.isEmpty(password) && !isValidPassword(password)){
+            mPassword?.error = getString(R.string.error_invalid_password)
+            focusView = mPassword
+            cancel = true
+
+        }else if(!isEmailValid(email)){
+            eMail?.error = getString(R.string.error_invalid_email)
+            focusView = eMail
+            cancel = true
+        }
+
+        if(cancel){
+            focusView?.requestFocus()
+        }else {
+            createFirebaseUser()
+        }
+
+    }
+
+    private fun isEmailValid(email: String):Boolean{
+        return email.contains("@")
+    }
+
+    private fun isValidPassword(password: String):Boolean{
+        var confirmPassword = mConfirmPassword?.text!!.toString()
+        return confirmPassword.equals(password) && password.length > 6
+    }
+
+
+
+    fun createFirebaseUser(){
+
+        var email: String = eMail?.text!!.toString()
+        var passowrd: String = mPassword?.text!!.toString()
+
+        mAuth?.createUserWithEmailAndPassword(email,passowrd)!!.addOnCompleteListener(this){
+            task ->
+            Log.d("Firebase","createUser onComplete : " + task.isSuccessful())
+
+            if(!task.isSuccessful){
+                Log.d("Firebase","user creation failed")
+                showErroeDialog("Registration attempt failed")
+            }else{
+                var intent = Intent(this@RegisterActivity,LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    private fun showErroeDialog(message: String){
+        var alert = AlertDialog.Builder(this)
+                .setTitle("Oops!")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok,null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
+    }
+
+
+}
